65e47605ae72316a0940c63ba0541e7750816b72
cypress <motolegacy.git@gmail.com>
2026-09-12 15:14:16 -0700

    Separate Ray Gun and Mark II Effects (#176)

--- tools/qc-compiler-gnu.sh
+++ tools/qc-compiler-gnu.sh
@@ -93,9 +93,6 @@ function compile_progs()
 function main()
 {
 	setup;
-	compile_progs "csqc" "FTE CSQC" "-DFTE -Wall"
-	compile_progs "ssqc" "FTE SSQC" "-O3 -DFTE ${TEST_FLAG} -Wall"
-	compile_progs "menu" "FTE MenuQC" "-O3 -DFTE -Wall"
 	compile_progs "ssqc" "Vril SSQC" "-O3 -Ono-compound_jumps -Wall"
 	exit ${RC}
 }
